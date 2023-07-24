56ecddb2d43f9f01141206cfd0a3461756149926
Athlmohn <athulmnair01@gmail.com>
2023-07-24 10:23:05 +0530

    second commit

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Weather App',
+  title: 'Sky Pilot',
   description: 'Your Personal whether wizard!',
 }
 
